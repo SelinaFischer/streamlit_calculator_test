7289a264bc07400a0a7ad8fa8d7b6fe0af4b83b9
Selina Fischer <selinafishx@gmail.com>
2025-07-15 22:21:07 +0100

    update streamlit calc code

--- Streamlit_calculator.py
+++ Streamlit_calculator.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.title("Simple Calculator")
 
-num1 = st.number_input("Enter first number", value=0)
-num2 = st.number_input("Enter second number", value=0)
+num1 = st.number_input("Enter first number")
+num2 = st.number_input("Enter second number")
 
 operation = st.selectbox("Select operation", ["Add", "Subtract", "Multiply", "Divide"])
 
